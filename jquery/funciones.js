4239edc405bea6d7e0e91af9fd50ade22b451247
maojust <mauriciogalvis77@gmail.com>
2020-08-02 19:05:47 -0500

    primer commit

---
+++ jquery/funciones.js
@@ -0,0 +1,25 @@
+
+  $(document).ready(function(){
+    //$('li').css('background', 'yellow');
+   
+  
+    $('#otrapag').click(function(){
+       alert('quieres ir a la pagina enefermedades');
+       
+        });
+        $('#redsocial').click(function(){
+            alert('SEGURO QUIERES SALIR DE LA PAGINA');
+           
+            });
+            $('#redsocial1').click(function(){
+              alert('SEGURO QUIERES SALIR DE LA PAGINA');
+             
+              });
+              $('#redsocial2').click(function(){
+                alert('SEGURO QUIERES SALIR DE LA PAGINA');
+               
+                });
+           
+                });
+                
+               
